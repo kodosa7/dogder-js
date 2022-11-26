760e4638c017566a4bc8fbe90ec61de2153d2294
ELS <57393100+kodosa7@users.noreply.github.com>
2022-11-26 08:50:27 +0100

    first commit

---
+++ Dog.js
@@ -0,0 +1,34 @@
+class Dog {
+    constructor(myData) {
+        Object.assign(this, myData)
+        this.dogCnt = 0
+    }
+
+    renderDog() {
+        document.getElementById('entry').innerHTML = 
+        `
+        <div id="avatar">
+            <div id="badge"></div>
+            <div id="labels">
+                <h1>
+                    <span id="name">${this.name}, </span>
+                    <span id="age">${this.age}</span>
+                </h1>
+                <h2>
+                    <span id="bio">${this.bio}</span>
+                </h2>
+            </div>
+        </div>
+        `
+        document.getElementById('avatar').style.backgroundImage = `url(${this.avatar}')`
+    }
+
+    renderBadge() {
+        document.getElementById('badge').innerHTML =
+        `
+        <img src="${this.badge}">
+        `
+    }
+}
+
+export default Dog
---
+++ index.js
@@ -0,0 +1,97 @@
+import data from './data.js'
+import Dog from '/Dog.js'
+
+const myData = {}
+Object.assign(myData, data)
+
+const currentDog = new Dog(myData)
+const heartBtn = document.getElementById('heart-btn')
+const crossBtn = document.getElementById('cross-btn')
+
+// initialization state
+const init = () => {
+    const currentDog = new Dog(myData[0])
+    currentDog.renderDog()
+}
+init()
+
+
+// click on heart (like) button
+const likeDog = () => {
+    renderLike()
+    delete myData[currentDog.dogCnt]
+    waitAndShowNextDog(500)
+}
+
+// click on cross (dislike) button
+const dislikeDog = () => {
+    renderNope()
+    delete myData[currentDog.dogCnt]
+    waitAndShowNextDog(500)
+}
+
+// render next dog in the queue
+const showNextDog = () => {
+    currentDog.dogCnt++
+    
+    if (currentDog.dogCnt >= data.length) {
+        currentDog.dogCnt = 0
+    }
+    const myDog = new Dog(myData[currentDog.dogCnt])
+    myDog.renderDog()
+}
+
+// render LIKE badge
+const renderLike = () => {
+    currentDog.badge = 'images/badge-like.png'
+    currentDog.renderBadge()
+}
+
+// render NOPE badge
+const renderNope = () => {
+    currentDog.badge = 'images/badge-nope.png'
+    currentDog.renderBadge()
+}
+
+const waitAndShowNextDog = (milliseconds) => {
+    removeClickables()
+    
+    setTimeout(function() {
+        restoreClickables()
+        
+        // test whether the dogs object is empty
+        if (Object.keys(myData).length === 0) {
+            renderNoMoreDogs()
+        } else {
+            showNextDog()
+        }
+    }, milliseconds)
+}
+
+const renderNoMoreDogs = () => {
+    document.getElementById('entry').innerHTML =
+    `
+    <div id="no-more-dogs">
+    <h1>No more dogs ☹️</h1>
+    </div>
+    `
+    removeClickables()
+}
+
+const removeClickables = () => {
+    heartBtn.removeEventListener('click', likeDog)
+    crossBtn.removeEventListener('click', dislikeDog)
+    heartBtn.style.opacity = '.15'
+    crossBtn.style.opacity = '.15'
+}
+
+const restoreClickables = () => {
+    heartBtn.addEventListener('click', likeDog)
+    crossBtn.addEventListener('click', dislikeDog)
+    heartBtn.style.opacity = '1'
+    crossBtn.style.opacity = '1'
+}
+
+// add listeners
+heartBtn.addEventListener('click', likeDog)
+crossBtn.addEventListener('click', dislikeDog)
